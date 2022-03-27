4a0f6c0e9d700215809d1169f41321167066be4f
Ryannnkl <ryannnkl@gmail.com>
2022-03-27 00:24:56 -0300

    Performance e flatpak
    agora os programas são armazenados em um arquivo txt pra listar
    apenas uma vez, tanto o flatpak tanto os apps dnf

--- pos_install.sh
+++ pos_install.sh
@@ -10,8 +10,13 @@ AMARELO='\e[1;33m'
 AZUL='\e[1;36m'
 
 package_update(){
-  echo -e "${AZUL}[atualizando] - verificando se a atualizações pendentes"
+  echo -e "${AZUL}[atualizando]${SEM_COR} - verificando se a atualizações pendentes"
   sudo dnf update -y
+}
+
+
+programs_instaled() {
+  cat programas.txt | grep -q "$@"
 }
 
 # -------------------------------TESTES E REQUISITOS----------------------------------------- #
@@ -26,6 +31,12 @@ fi
 
 # ------------------------------- INSTALAÇÃO DE PACOTES DNF ----------------------------------------- #
 
+echo -e "${AZUL}[DNF]${SEM_COR} configurando e instalando dnf's"
+
+package_update
+
+dnf list --installed > programas.txt
+
 PROGRAMAS_PARA_INSTALAR=(
   neovim
   nodejs
@@ -37,25 +48,55 @@ PROGRAMAS_PARA_INSTALAR=(
 )
 
 for nome_do_programa in ${PROGRAMAS_PARA_INSTALAR[@]}; do
-  if ! dnf list --installed | grep -q $nome_do_programa; then # Só instala se já não estiver instalado
+  if ! programs_instaled $nome_do_programa; then # Só instala se já não estiver instalado
     sudo dnf install "$nome_do_programa" -y
   else
-    echo -e "${AMARELO}[INSTALADO]${SEM_COR} - $nome_do_programa"
+    echo -e "${AMARELO}[INSTALADO]${SEM_COR} [dnf] - $nome_do_programa"
   fi
 done
+
 
 # ------------------------------- CONFIGURANDO FLATPAK ----------------------------------------- #
 
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
+FLATPAK_APPS=(
+  org.inkscape.Inkscape
+  org.telegram.desktop
+  com.simplenote.Simplenote
+  com.discordapp.Discord
+  de.haeckerfelix.Fragments
+  org.eclipse.Java
+  org.gimp.GIMP
+  sh.ppy.osu
+)
+
+echo -e "${AZUL}[FLATPAK]${SEM_COR} configurando e instalando flatpaks"
+
+flatpak list > flatpaks.txt
+
+for flatpak in ${FLATPAK_APPS[@]}; do
+  if ! cat flatpaks.txt | grep -q $flatpak; then
+    flatpak install $flatpak -y
+     echo -e "${AMARELO}[INSTALADO]${SEM_COR} [flatpak] - $flatpak"
+  else 
+     echo -e "${AMARELO}[INSTALADO]${SEM_COR} [flatpak] - $flatpak"
+  fi
+done
+
 # ------------------------------- CONFIGURANDO NVIM ----------------------------------------- #
 
 if ls /home/$USER/.config/nvim; then
-  echo -e "${AMARELO}[INFO]${SEM_COR} Diretorio já criado"
-  else
-    git clone https://github.com/Ryannnkl/vim-config.git /home/$USER/.config/nvim
+ echo -e "${AMARELO}[INFO]${SEM_COR} Diretorio já criado"
+ else
+   git clone https://github.com/Ryannnkl/vim-config.git /home/$USER/.config/nvim
 fi
 
 # package_update
 
+# ------------------------------- REMOVENDO ARQUIVOS TEMPORARIOS ----------------------------------------- #
+
+rm programas.txt flatpaks.txt
+
 echo -e "${VERDE}[INFO] - Script finalizado, instalação concluída! :)${SEM_COR}"
+
